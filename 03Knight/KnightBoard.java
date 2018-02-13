fecc687cc3d7c93ff6390bac04d4b87505060bdc
AhnafK <akazi1@stuy.edu>
2018-02-13 15:30:24 -0500

    failure to step

--- 03Knight/KnightBoard.java
+++ 03Knight/KnightBoard.java
@@ -1,5 +1,5 @@
 public class KnightBoard{
-    int[][] board;
+    private int[][] board;
     
     public KnightBoard(int rows, int cols){
 	board = new int[rows][cols];
@@ -13,7 +13,7 @@ public class KnightBoard{
     public String toString(){
 	String ans = "";
 	for(int x = 0; x < board.length; x++){
-	    for(int y = 0; y < board.length; y++){
+	    for(int y = 0; y < board[0].length; y++){
 		if((""+board[x][y]).length()==1)
 		    ans += ' ';
 		ans += board[x][y] + " ";
@@ -23,6 +23,16 @@ public class KnightBoard{
 	return ans;
     }
 
-
+    private boolean step(int x, int y, int pos){
+	if(pos == 0 &&){
+	    board[x+2][y+1] = board[x][y] + 1;
+	}
+	if 
+    }
     
+
+    public static void main(String[]args){
+	KnightBoard f = new KnightBoard(7,5);
+	System.out.println("" + f);
+    }
 }
