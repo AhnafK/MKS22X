ab40b3d09837bf9b950404f266f6a2ec0046793c
AhnafK <akazi1@stuy.edu>
2018-02-15 15:27:37 -0500

    wow

--- 03Knight/KnightBoard.java
+++ 03Knight/KnightBoard.java
@@ -23,22 +23,69 @@ public class KnightBoard{
 	return ans;
     }
     
-    private boolean step(int x, int y, int pos){
+    private boolean step(int x, int y, int pos, int j){
 	if(pos == 0)
-	    goodStep(x,y,2,1);
+	    return goodStep(x,y,2,1,j);
+	if(pos == 1)
+	    return goodStep(x,y,1,2,j);
+	if(pos == 2)
+	    return goodStep(x,y,-1,2,j);
+	if(pos == 3)
+	    return goodStep(x,y,-2,1,j);
+	if(pos == 4)
+	    return goodStep(x,y,-2,-1,j);
+	if(pos == 5)
+	    return goodStep(x,y,-1,-2,j);
+	if(pos == 6)
+	    return goodStep(x,y,1,-2,j);
+	return goodStep(x,y,2,-1,j);
     }
     
-    private boolean goodStep(int x, int y, int ex, int why){
+    private boolean goodStep(int x, int y, int ex, int why, int j){
 	if(x+ex < board.length && x+ex >= 0 && y+why < board[0].length && y+why >= 0){
-	    board[x+ex][y+why] += 1;
+	    board[x+ex][y+why] += j;
 	    return true;
 	}
 	return false;
     }
 
+    private boolean solveF(int x, int y, int level, int f){
+	if(x == 0)
+	    return solveH(x+2,y+1,level+1);
+	if(x == 1)
+	    return solveH(x+1,y+2,level+1);
+	if(x == 2)
+	    return solveH(x-1,y+2,level+1);
+	if(x == 3)
+	    return solveH(x-2,y+1,level+1);
+	if(x == 4)
+	    return solveH(x-2,y-1,level+1);
+	if(x == 5)
+	    return solveH(x-1,y-2,level+1);
+	if(x == 6)
+	    return solveH(x+1,y-2,level+1);
+	return solveH(x+2,y-1,level+1);
+    }
+    
+    public boolean solve(int startingRows,int startingCols){
+	board[startingRows][startingCols] = 1;
+	return solveH(startingRows,startingCols,2);
+    }
+
+    private boolean solveH(int row ,int col, int level){
+	if(level == (board.length * board[0].length))
+	    return true;
+	for(int x = 0; x < 8; x++){
+	    if(step(row,col,x,level)){
+		return solveF(row, col, level, x);
+	    }
+	}
+	return false;
+    }
     
     public static void main(String[]args){
-	KnightBoard f = new KnightBoard(7,5);
+	KnightBoard f = new KnightBoard(5,5);
+	f.solve(0,0);
 	System.out.println("" + f);
     }
 }
