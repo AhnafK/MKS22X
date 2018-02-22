3cd09bc9d301be5ff2c67a09ae0bbb1ade44a101
Ahnaf Kazi <akazi1@stuy.edu>
2018-02-21 20:21:56 -0500

    finished assignment woot. Fixed a typo that broke everything and made a badstep for undoing

--- 03Knight/KnightBoard.java
+++ 03Knight/KnightBoard.java
@@ -42,27 +42,53 @@ public class KnightBoard{
     }
     
     private boolean goodStep(int x, int y, int ex, int why, int j){
+	if(x+ex < board.length && x+ex >= 0 && y+why < board[0].length && y+why >= 0 && board[x+ex][y+why] == 0){
+	    board[x+ex][y+why] = j;
+	    return true;
+	}
+	return false;
+    }
+
+     private boolean unStep(int x, int y, int pos){
+	if(pos == 0)
+	    return badStep(x,y,2,1);
+	if(pos == 1)
+	    return badStep(x,y,1,2);
+	if(pos == 2)
+	    return badStep(x,y,-1,2);
+	if(pos == 3)
+	    return badStep(x,y,-2,1);
+	if(pos == 4)
+	    return badStep(x,y,-2,-1);
+	if(pos == 5)
+	    return badStep(x,y,-1,-2);
+	if(pos == 6)
+	    return badStep(x,y,1,-2);
+	return badStep(x,y,2,-1);
+    }
+    
+    private boolean badStep(int x, int y, int ex, int why){
 	if(x+ex < board.length && x+ex >= 0 && y+why < board[0].length && y+why >= 0){
-	    board[x+ex][y+why] += j;
+	    board[x+ex][y+why] = 0;
 	    return true;
 	}
 	return false;
     }
 
     private boolean solveF(int x, int y, int level, int f){
-	if(x == 0)
+	if(f == 0)
 	    return solveH(x+2,y+1,level+1);
-	if(x == 1)
+	if(f == 1)
 	    return solveH(x+1,y+2,level+1);
-	if(x == 2)
+	if(f == 2)
 	    return solveH(x-1,y+2,level+1);
-	if(x == 3)
+	if(f == 3)
 	    return solveH(x-2,y+1,level+1);
-	if(x == 4)
+	if(f == 4)
 	    return solveH(x-2,y-1,level+1);
-	if(x == 5)
+	if(f == 5)
 	    return solveH(x-1,y-2,level+1);
-	if(x == 6)
+	if(f == 6)
 	    return solveH(x+1,y-2,level+1);
 	return solveH(x+2,y-1,level+1);
     }
@@ -73,11 +99,19 @@ public class KnightBoard{
     }
 
     private boolean solveH(int row ,int col, int level){
-	if(level == (board.length * board[0].length))
+	//	System.out.println(""+ board.length + " " + board[0].length + " " + level+" " + board.length*board[0].length  + "\n_____________________________________");
+	if(level-1 == (board.length * board[0].length)){
 	    return true;
+	}
 	for(int x = 0; x < 8; x++){
 	    if(step(row,col,x,level)){
-		return solveF(row, col, level, x);
+		//System.out.println(this);
+		if(solveF(row, col, level, x))
+		    return true;
+		else{
+		    unStep(row, col, x);
+		    //System.out.println(this);
+		}
 	    }
 	}
 	return false;
@@ -85,7 +119,7 @@ public class KnightBoard{
     
     public static void main(String[]args){
 	KnightBoard f = new KnightBoard(5,5);
-	f.solve(0,0);
-	System.out.println("" + f);
+	if(f.solve(0,0))
+	    System.out.println("YAY \n" + f);
     }
 }
