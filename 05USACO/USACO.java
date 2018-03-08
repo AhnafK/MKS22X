c68043e9b23648c5d54dbbdde1837dad7322ae1c
AhnafK <akazi1@stuy.edu>
2018-03-08 15:30:01 -0500

    ???

--- 05USACO/USACO.java
+++ 05USACO/USACO.java
@@ -1,15 +1,26 @@
 public class USACO{
 
-    public static int bronzeHO(int)
+    public static int bronzeHO(int R, int C, int N, int[][] P){
+	int max = 0;
+	int may = 0;
+	for(int x = R-1; x < R+2; x++){
+	    for(int y = C-1; y < C-2; y++)
+		if(P[x][y] >= P[max][may]){
+		    max = x;
+		    may = y;
+		}
+	}
+	return bronzeO(R,C,N,P,max,may);
+    }
 
 
+    public static int bronzeO(int R, int C, int N, int[][] P, int Rm, int Cm){
+	if(N == 0){
+	    
+	}
+    }
+    
+    
 
-
-
-
-
-
-
-
-
+    
 }
