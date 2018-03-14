ec572c4f0098686d8b2c972e73cdd3f8dfcf92ff
Ahnaf Kazi <akazi1@stuy.edu>
2018-03-13 22:11:43 -0400

    corrected a 1 to be start in order to stop exceptions

--- 06QuickSelect/Quick.java
+++ 06QuickSelect/Quick.java
@@ -14,7 +14,7 @@ public class Quick{
 	
 	int small = start+1;
 	int large = end;
-	for(int x = 1; x <= end; x++){
+	for(int x = start; x <= end; x++){
 	    if(data[x] < pivot){
 		int keep = data[small];
 		data[small] = data[x];
@@ -32,7 +32,7 @@ public class Quick{
     
     public static void main(String[]args){
 	int[] f = { 999,999,999,4,1,0,3,2,999,999,999};
-	System.out.println(""+partition(f  , 0, 10));
+	System.out.println(""+partition(f  , 4, 10));
 	//partition(f);
     }
 
