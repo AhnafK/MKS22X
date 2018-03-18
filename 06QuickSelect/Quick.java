eda04972992dd51b82cfd74146fc5156f7681b05
Ahnaf Kazi <akazi1@stuy.edu>
2018-03-18 00:08:06 -0400

    Fixed a typo that broke everything for duplicates

--- 06QuickSelect/Quick.java
+++ 06QuickSelect/Quick.java
@@ -95,16 +95,16 @@ public class Quick{
     public static void sortH(int[]data, int hi, int lo, int[] last){
 	System.out.println("last: "+Arrays.toString(last) + " hi: "+ hi + " lo: "+lo);
 	if(last[1]+1 < hi)
-	    sortH(data,hi,last[0], partition(data,last[1]+1,hi));
+	    sortH(data,hi,last[1], partition(data,last[1]+1,hi));
 	if(last[0]-1 > lo)
-	    sortH(data,last[1],lo, partition(data,lo, last[0]-1));
+	    sortH(data,last[0],lo, partition(data,lo, last[0]-1));
     }
 
     public static void main(String[]args){
 	//int[]ary = { 2, 10, 15, 23, 0,  5};
 	//System.out.println("ans: "+select(ary, 4));
         
-	int[]arry = { 2, 10, 15, 23, 0,  5};
+	int[]arry = { 2,2,2,10,10,11,11,12,23,54545,-3,2, 10, 15, 23, 0,  5};
 	quickSort(arry);
 	System.out.println(Arrays.toString(arry));
     }
