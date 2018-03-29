389659e3210d8e58120765f799ffad32d435fe9f
AhnafK <akazi1@stuy.edu>
2018-03-29 15:25:17 -0400

    add to end, get, set Node, clear, indexOf

--- 08ZeldaedLists/MyLinkedList.java
+++ 08ZeldaedLists/MyLinkedList.java
@@ -4,7 +4,7 @@ public  class MyLinkedList{
 	private Node next;
 	private Integer data;
 
-	private Node(){};
+	private Node(){}
 	
 	private Node(Node l, Node n, Integer i){
 	    prev = l;
@@ -14,6 +14,11 @@ public  class MyLinkedList{
 	
 	private Node(Node n, Integer i){
 	    next = n;
+	    data = i;
+	}
+
+	private Node(Integer i, Node n){
+	    prev = n;
 	    data = i;
 	}
 
@@ -28,6 +33,16 @@ public  class MyLinkedList{
 	private Integer getValue(){
 	    return data;
 	}
+
+	private boolean setPrev(Node n){
+	    prev = n;
+	    return true;
+	}
+
+	private boolean setNext(Node n){
+	    next = n;
+	    return true;
+	}
     }
     private Node start;
     private Node end;
@@ -35,12 +50,14 @@ public  class MyLinkedList{
 
     public MyLinkedList(){
 	size = 0;
-	start = new Node();
-	end = new Node();
+	start = new Node(end, null);
+	end = new Node(null, start);
     }
 
     public boolean add(Integer value){
-	start.next = new Node(start, value);
+	Node neW = new Node(end, value);
+	end.getPrev().setNext(neW);
+	end.setPrev(neW);
 	size += 1;
 	return true;
     }
@@ -52,11 +69,49 @@ public  class MyLinkedList{
     public String toString(){
 	Node current = start.next;
 	String ans = "[";
-	while(current.next != end){
+	while(current != end){
 	    ans += " " + current.getValue() + ",";
+	    current = current.getNext();
 	}
 	return ans + "]";
     }
 
+    public Integer get(int index){
+	if(index >= size){
+	    throw new IndexOutOfBoundsException();
+	}
+	Node current = start.next;
+	for(int x = 0; x < index; x ++){
+	    current = current.getNext();
+	}
+	return current.getValue();
+    }
+
+    public int indexOf(Integer value){
+	Node current = start.getNext();
+	int x = 0;
+	while(current.getValue() != value){
+	    x+=1;
+	    current = current.getNext();
+	}
+	return x;
+    }
+
+    public void clear(){
+	size = 0;
+	start.setNext(end);
+	end.setPrev(start);
+    }
     
+    public static void main(String[]args){
+	MyLinkedList data = new MyLinkedList();
+	
+	data.add(4);
+	data.add(5);
+	data.add(6);
+	System.out.println(data);
+	System.out.println(""+data.get(1) + " " + data.indexOf(6) + " " + data.size());
+	data.clear();
+	System.out.println(data);
+    }
 }
