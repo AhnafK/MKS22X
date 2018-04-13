d4a2bfe26f067faa7343044a12d19dc76396a42c
AhnafK <akazi1@stuy.edu>
2018-04-13 15:35:30 -0400

    stuff? it compiles

--- 08ZeldaedLists/Sorts.java
+++ 08ZeldaedLists/Sorts.java
@@ -1,11 +1,22 @@
+import java.util.*;
 public class Sorts{
 
     public static void radixsort(MyLinkedListImproved<Integer> data){
-	//Literator L = data.iterator();
+	MyLinkedListImproved<Integer>.Literator L = data.iterator();
 	int g = (""+data.max()).length();
-	
-
+	@SuppressWarnings("unchecked") MyLinkedListImproved<Integer>[] whoa= new MyLinkedListImproved[10];
+	for(int poopy = 0; poopy < g; poopy++){
+	    L = data.iterator();
+	    while(L.hasNext()){
+		Integer Pres = L.next();
+		whoa[Pres/(int)Math.pow(10,poopy)%10].add(Pres);
+	    }
+	    data = whoa[0];
+	    for(int x = 1; x < 10; x++){
+		data.extend(whoa[x]);
+	    }
+	}
     }
-
-
+    
+    
 }
