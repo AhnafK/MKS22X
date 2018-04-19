07de4b3e08001edaa196a35ec0d1812956934131
AhnafK <akazi1@stuy.edu>
2018-04-19 15:35:10 -0400

    work was actually done, 2 number addition

--- 09PostFix/Calculator.java
+++ 09PostFix/Calculator.java
@@ -1,39 +1,73 @@
 import java.util.*;
 public class Calculator{
-        private class Stack{
-	private LinkedList<String> stak;
+        // private class Stack{
+    // 	private LinkedList<String> stak;
 
-	private stack(String enter){
-	    stak = new LinkedList<String>;
-	    String now = "";
-	    for(int x = enter.length(); x < enter.length(); x++){
-		if(enter.charAt(x) == ' ' || x = enter.length()-1){
-		    stak.add(now);
-		    now = "";
-		}
+    // 	private stack(String enter){
+    // 	    stak = new LinkedList<String>;
+    // 	    String now = "";
+    // 	    for(int x = enter.length(); x < enter.length(); x++){
+    // 		if(enter.charAt(x) == ' ' || x = enter.length()-1){
+    // 		    stak.add(now);
+    // 		    now = "";
+    // 		}
+    // 	    }
+    // 	}
+
+    // 	private void push(String l){
+    // 	    stak.add(l);
+    // 	}
+
+    // 	private String pop(){
+    // 	    return stak.pop();
+    // 	}
+	
+    // }
+    public static LinkedList<String> stack(String enter){
+	LinkedList<String> stak = new LinkedList<String>();
+	String now = "";
+	for(int x = 0; x < enter.length(); x++){
+	    if(enter.charAt(x) == ' '){
+		
+		stak.add(now);
+		//System.out.println(now);
+		now = "";
+	    }
+	    else{
+		now+=enter.charAt(x);
 	    }
 	}
-
-	private void push(String l){
-	    stak.add(l);
-	}
-
-	private String pop(){
-	    return stak.pop();
-	}
-	
+	stak.add(now);
+	return stak;
     }
+    
 
     public static double eval(String s){
-	stack gravy = new stack(s);
-	ListIterator<String> = gravy.ListIterator();
-	while(gravy.hasNext()){
-	    double f = (double)gravy.next();
-	    double d = (double)gravy.next();
-	    String g = gravy.next();
-	    if("+".equals(g)){
-		f + d
+	LinkedList<String> gravy = stack(s);
+	System.out.println("first: "+gravy);
+	while(gravy.size() > 1){
+	    double a = Double.parseDouble(gravy.pop());
+	    System.out.println("a "+a+" "+gravy);
+	    double b = Double.parseDouble(gravy.pop());
+	    System.out.println("b "+b+" "+gravy);
+	    //while(gravy.size() > 0){
+	    String c = gravy.pop();
+	    System.out.println("|"+c+"|");
+	    if(c.equals("+"))
+		gravy.push(a+b+"");
+	    else if(c.equals("-"))
+		gravy.push(a-b+"");
+	    else if(c.equals("/"))
+		gravy.push(a-b+"");
+	    else{
+		gravy.push(b+"");
+		gravy.push(a+"");
 	    }
+	    System.out.println(gravy);
 	}
+	return Double.parseDouble(gravy.pop());
+    }
+    public static void main(String[]args){
+	System.out.println(""+eval("3 34 +"));
     }
 }
