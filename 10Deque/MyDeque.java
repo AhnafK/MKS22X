609adc29c6504cd1a7febebc90127502a8323cd0
Ahnaf Kazi <akazi1@stuy.edu>
2018-04-24 01:15:06 -0400

    finished

--- 10Deque/MyDeque.java
+++ 10Deque/MyDeque.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class MyDeque<E>{
     private int start;
     private int size;
@@ -23,20 +24,32 @@ public class MyDeque<E>{
 	return size;
     }
 
+    /*
     @SuppressWarnings("unchecked")
     private void resize(){
 	E[] hold = (E[])new Object[size*2];
-	
+	int phase = hold.length/2;
+	for(int x = 0; x < size; x++){
+	    
+	}
+    }
+    */
+    
+    private void resize(){
+	MyDeque<E> pres = new MyDeque<>(size*2);
+	for(int x = 0; x < size; x++){
+	    pres.addLast(removeFirst());
+	}
     }
     
     public void addFirst(E x){
 	if(size == deq.length){
 	    resize();
 	}
-	if(start == 0)
+	if(start == 0){
 	    start = deq.length-1;
 	    deq[deq.length-1] = x;
-	
+	}
 	else{
 	    start -= 1;
 	    deq[start] = x;
@@ -46,36 +59,67 @@ public class MyDeque<E>{
 	
     }
 
-    public void removeFirst(E x){
+    public E removeFirst(){
 	E ans = null;
 	if(size == 0){
 	    throw new NoSuchElementException();
 	}
-	if(start == 0){
+	if(start == deq.length-1){
 	    ans = deq[deq.length-1];
-	    deq[deq.legnth-1]=null;
+	    start = 0;
 	}
 	else{
-	    start -= 1;
-	    deq[start] = null;
-	    
+	    ans = deq[start];
+	    start += 1;
 	}
 	size-=1;
-	
+	return ans;
     }
-
+    
+    public E removeLast(){
+	E ans = null;
+	if(size == 0){
+	    throw new NoSuchElementException();
+	}
+	//System.out.println("Poop: "+Arrays.toString(deq));
+	ans = deq[(size + start-1)%deq.length];
+	    
+	size-=1;
+	return ans;
+    }
+    
     public void addLast(E x){
 	if(size == deq.length){
 	    resize();
 	}
-	if(start+size-1 == deq.length-1)
-	    deq[0] = x;
-	else{
-	    deq[start+size]=x;
-	}
+	deq[(start+size)%deq.length]=x;
 	size+=1;
     }
 
 
+    public String toString(){
+	String ans = "[";
+	for(int x = 0; x < size; x++){
+	    ans += deq[(start+x)%deq.length]+", ";
+	}
+	return ans + "]";
+    }
 
+    public E getFirst(){
+	return deq[start];
+    }
+    
+    public E getLast(){
+	return deq[(start+size-1)%deq.length];
+    }
+    
+    public static void main(String[]args){
+	MyDeque<Integer> f = new MyDeque<>(4);
+	f.addFirst(4);
+	System.out.println(""+f.size());
+	f.addFirst(10);System.out.println(""+f.size()+ " " + f);f.addFirst(124);
+	f.addLast(142);System.out.println(""+f.size() + f);
+	System.out.println(f.removeFirst()+ " " + f + " " + f.removeLast());
+	System.out.println(f);
+    }
 }
