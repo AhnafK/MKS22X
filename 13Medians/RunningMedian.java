e8eb32159fdb547e6607d1dc0cfdc2d7afb4c22a
Ahnaf Kazi <akazi1@stuy.edu>
2018-05-02 00:28:35 -0400

    screw this

--- 13Medians/RunningMedian.java
+++ 13Medians/RunningMedian.java
@@ -1,3 +1,5 @@
+//import java.util.*;
+
 public class RunningMedian{
     private MyHeap<Double> small;
     private MyHeap<Double> large;
@@ -8,18 +10,39 @@ public class RunningMedian{
     }
     
     public void add(Double a){
-	if(a<getMedian()){
+	System.out.println("\n\n"+large + "\n" + small + "\n\n");
+	if(large.size()==0){
+	    large.add(a);
+	    System.out.println("\n\n"+large + "\n" + small + "\n\n____");
+	}
+	else if(large.size()-small.size()>1/*||(large.size() > small.size() &&)*/){
+	    large.add(a);
+	    small.add(large.remove());
+	    System.out.println("\n\n"+large + "\n" + small + "\n\n____");
+	    return;
+	}
+	else if(a<getMedian()){
 	    small.add(a);
+	    System.out.println("\n\n"+large + "\n" + small + "\n\n____");
+	    return;
 	}
 	else{
+	    if(large.size()-small.size()>1/*||(large.size() > small.size() &&)*/){
+		large.add(a);
+		small.add(large.remove());
+		System.out.println("\n\n"+large + "\n" + small + "\n\n____");
+		return;
+	    }
 	    large.add(a);
+	    System.out.println("\n\n"+large + "\n" + small + "\n\n____");
+	    return;
 	}
-	if(large.size()-1>small.size()){
-	    
-	}
+	
     }
     
     public Double getMedian(){
+	//System.out.println(large.size()+"");
+	//System.out.println(small.size()+"");
 	if(large.size() > small.size()){
 	    return large.peek();
 	}
@@ -31,7 +54,9 @@ public class RunningMedian{
     public static void main(String[]args){
 	RunningMedian f = new RunningMedian();
 	f.add(123.);
-	f.add
-	System.out.println(""+f.getMedian());
+	f.add(536.);
+	f.add(2.);
+	f.add(43524.);
+	System.out.println(""+f.small + "||"+ f.large+ f.getMedian());
     }
 }
